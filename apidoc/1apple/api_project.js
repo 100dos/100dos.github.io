a9b650bd6d7d4689c07be64bcffa349cbc9f3352
Ji Gang <ji.xiaod@gmail.com>
2020-11-06 15:05:39 +0800

    fixupdate

--- apidoc/1apple/api_project.js
+++ apidoc/1apple/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-03T01:48:31.245Z",
+    "time": "2020-11-06T07:05:24.494Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
